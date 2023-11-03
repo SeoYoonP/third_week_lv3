0a6ec78c40f8db09b7ee17f9c349ace8aac57480
Seoyoon <wideskyinme@gmail.com>
2023-11-04 01:37:40 +0900

    REFACTOR: ErrorCode 메세지 수정

--- src/main/java/com/example/springassignmentlv3/exception/ErrorCode.java
+++ src/main/java/com/example/springassignmentlv3/exception/ErrorCode.java
@@ -8,30 +8,23 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    EMAIL_DIFFERENT_FORMAT(HttpStatus.BAD_REQUEST.value(), "이메일 형식이 일치하지 않습니다."),
-    NICKNAME_DIFFERENT_FORMAT(HttpStatus.BAD_REQUEST.value(), "닉네임 형식이 일치하지 않습니다."),
-    PASSWORD_DIFFERENT_FORMAT(HttpStatus.BAD_REQUEST.value(), "비밀번호 형식이 일치하지 않습니다."),
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "이미 이메일이 존재합니다."),
-    NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "이미 닉네임이 존재합니다."),
-    INVALID_EMAIL_PASSWORD(HttpStatus.BAD_REQUEST.value(), "잘못된 이메일이거나 잘못된 비밀번호입니다."),
-    INVALID_DEPARTMENT(HttpStatus.BAD_REQUEST.value(), "접근할 수 없는 부서입니다."),
-    NOT_FOUND_USER(HttpStatus.BAD_REQUEST.value(), "사용자를 찾을 수 없습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(), "토큰 유효기간 만료."),
-    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "리프레쉬토큰 유효기간 만료."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효한 토큰이 아닙니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효한 리프레쉬토큰이 아닙니다."),
-    NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED.value(),"해당 엑세스 토큰이 존재하지 않습니다."),
-    NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(),"로그인 이후에 이용이 가능합니다."),
-    OUT_OF_RANGE(HttpStatus.BAD_REQUEST.value(), "범위를 벗어난 페이지 요청입니다."),
-    REVIEW_NOT_EXIST(HttpStatus.NOT_FOUND.value(), "리뷰가 존재하지 않습니다."),
-    TOKEN_NOT_EXIST(HttpStatus.UNAUTHORIZED.value(), "로그인이 필요한 기능입니다."),
-    NOT_THE_AUTHOR(HttpStatus.FORBIDDEN.value(), "작성자가 아닙니다."),
-    ELEMENTS_IS_REQUIRED(HttpStatus.BAD_REQUEST.value(), "필수값이 생략되었습니다."),
-    UPLOAD_FAILED(HttpStatus.BAD_REQUEST.value(), "파일 업로드를 실패했습니다."),
-    UNSUPPORTED_MEDIA_type(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), "지원하지 않는 파일 형식입니다."),
-    COMMENT_NOT_EXIST(HttpStatus.NOT_FOUND.value(), "댓글이 존재하지 않습니다."),
-    UNEXPECTED_ERROR(443, "예기치못한 오류"), ;
+    EMAIL_DIFFERENT_FORMAT(HttpStatus.BAD_REQUEST.value(), "이메일 형식이 올바르지 않습니다."),
+    PASSWORD_DIFFERENT_FORMAT(HttpStatus.BAD_REQUEST.value(), "비밀번호는 8~15자리로, 알파벳 대소문자, 숫자, 특수문자를 포함해야 합니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "이미 등록된 이메일입니다."),
+    INVALID_EMAIL_PASSWORD(HttpStatus.BAD_REQUEST.value(), "이메일 또는 비밀번호가 정확하지 않습니다."),
+    INVALID_DEPARTMENT(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 부서입니다."),
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST.value(), "해당 사용자를 찾을 수 없습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(), "토큰이 만료되었습니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "리프레시 토큰이 만료되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "토큰이 유효하지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "리프레시 토큰이 유효하지 않습니다."),
+    NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED.value(),"엑세스 토큰을 찾을 수 없습니다."),
+    NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(),"로그인 후 이용해 주세요."),
+    OUT_OF_RANGE(HttpStatus.BAD_REQUEST.value(), "요청한 페이지 범위가 적절하지 않습니다."),
+    TOKEN_NOT_EXIST(HttpStatus.UNAUTHORIZED.value(), "이 기능을 사용하기 위해서는 로그인이 필요합니다."),
+    ELEMENTS_IS_REQUIRED(HttpStatus.BAD_REQUEST.value(), "필수 입력 필드가 누락되었습니다."),
+    UNEXPECTED_ERROR(443, "예상치 못한 오류가 발생했습니다."),;
+
     private final int httpStatus;
     private final String message;
-
 }
